a5224d255e13c9180827ce373cc46f86bfb83b7c
kim-moai <xogks12345@naver.com>
2024-04-28 23:59:44 +0900

    11

--- src/App.js
+++ src/App.js
@@ -3,21 +3,21 @@ import PaymentForm from "./components/PaymentForm/PaymentForm";
 import Expenses from "./components/Payments/Expenses";
 
 function App() {
-	const expenses = [
-		{
-			id: "e1",
-			title: "수건",
-			amount: 12.33,
-			date: new Date(2025, 8, 14),
-		},
-	];
+  const expenses = [
+    {
+      id: "e1",
+      title: "수건1",
+      amount: 12.33,
+      date: new Date(2025, 8, 14),
+    },
+  ];
 
-	return (
-		<>
-			<PaymentForm />
-			<Expenses items={expenses} />
-		</>
-	);
+  return (
+    <>
+      <PaymentForm />
+      <Expenses items={expenses} />
+    </>
+  );
 }
 
 export default App;
